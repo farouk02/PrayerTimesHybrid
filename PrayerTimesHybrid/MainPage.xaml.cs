e48c868d71a6f36f2668cb192f493017a4e6e737
Farouk KIOUS <faroukious0@gmail.com>
2024-12-29 22:07:48 +0100

    Add project files.

---
+++ PrayerTimesHybrid/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrayerTimesHybrid
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
